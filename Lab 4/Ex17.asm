2a33241d087587c8ea82d0d66eea7e1ece198e55
Rares Stefan Dobre <rares.dobre@stud.ubbcluj.ro>
2024-11-04 08:34:51 +0200

    Solve lab4

--- Lab 4/Ex17.asm
+++ Lab 4/Ex17.asm
@@ -10,12 +10,48 @@ import exit msvcrt.dll  ; we specify the external library that defines the symbo
 ; our variables are declared here (the segment is called data)
 segment data use32 class=data
 ; ...
-
+a dw 1010110111010011b
+b db 00100101b
+c dd 0
 ; the program code will be part of a segment called code
 segment code use32 class=code
 start:
 ; ...
+mov ECX, 0
+mov AL, 00001111b
+or CL, AL
+;the bits 0-3 of C have the value 1
 
+mov AX, [a]
+mov DL, 00001111b
+and DL, AL
+shl DL, 4
+or CL, DL
+;the bits 4-7 of C are the same as the bits 0-3 of A
+
+mov CH, 0
+; the bits 8-13 of C have the value 0
+; The bits 14 and 15 also have the value 0 now but they will be overwritten
+
+mov EDX, 0
+mov DX, 0011111111110000b
+and DX, AX
+shl EDX, 10
+or EDX, ECX
+; the bits 14-23 of C are the same as the bits 4-13 of A
+
+mov EDX, 0
+mov DL, 11111100b
+shl EDX, 22
+or ECX, EDX
+; the bits 24-29 of C are the same as the bits 2-7 of B
+
+mov EDX, 00000011b
+shl EDX, 30
+or ECX, EDX
+; the bits 30-31 have the value 1
+
+mov [c], ECX
     ; call exit(0) ), 0 represents status code: SUCCESS
     push dword 0 ; saves on stack the parameter of the function exit
     call [exit] ; function exit is called in order to end the execution of the program
--- Lab 4/Ex2.asm
+++ Lab 4/Ex2.asm
@@ -10,12 +10,41 @@ import exit msvcrt.dll  ; we specify the external library that defines the symbo
 ; our variables are declared here (the segment is called data)
 segment data use32 class=data
 ; ...
-
+a dw 1010110111010011b
+b dw 1110101100100101b
+c dd 0
 ; the program code will be part of a segment called code
 segment code use32 class=code
 start:
 ; ...
+mov ECX, 0
+mov DX, 00000001_11100000b
+and DX, [b]
+shr DX, 5
+or CX, DX
+; the bits 0-3 of C are the same as the bits 5-8 of B
 
+mov DX, 00000000_00011111b
+and DX, [a]
+shl DX, 4
+or CX, DX
+; the bits 4-8 of C are the same as the bits 0-4 of A
+
+mov DX, 00011111_11000000b
+and DX, [a]
+shl DX, 3
+or CX, DX
+; the bits 9-15 of C are the same as the bits 6-12 of A
+
+
+mov EBX, 0
+mov BX, [b]
+shl EBX, 16
+or ECX, EBX
+; the bits 16-31 of C are the same as the bits of B
+
+
+mov [c], ECX
     ; call exit(0) ), 0 represents status code: SUCCESS
     push dword 0 ; saves on stack the parameter of the function exit
     call [exit] ; function exit is called in order to end the execution of the program
